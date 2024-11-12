021924ad2fd24a2868f28ba62eaf43f5768888f5
jangmi <jsy639@naver.com>
2024-11-12 16:43:05 +0900

    54페이지 게시글 수정요청(수정하고 돌아오면 원래가 있던 페이지까지 가게끔 해놓음)

--- springbootstudy-mybbs03/src/main/java/com/springbootstudy/bbs/controller/BoardController.java
+++ springbootstudy-mybbs03/src/main/java/com/springbootstudy/bbs/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.springbootstudy.bbs.controller;
 
 import java.io.PrintWriter;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springbootstudy.bbs.domain.Board;
 import com.springbootstudy.bbs.service.BoardService;
@@ -80,7 +82,8 @@ public class BoardController {
 	}
 	
 	@PostMapping("/updateForm")
-	public String updateBoard(Model model,HttpServletResponse response, PrintWriter out, @RequestParam("no") int no, @RequestParam("pass") String pass)
+	public String updateBoard(Model model,HttpServletResponse response, PrintWriter out, 
+			@RequestParam("no") int no, @RequestParam("pass") String pass,@RequestParam(value="pageNum",defaultValue = "1")int pageNum)
 	{
 		boolean isPassCheck=boardService.isPassCheck(no, pass);
 		if(!isPassCheck) {
@@ -93,11 +96,13 @@ public class BoardController {
 		}
 		Board board=boardService.getBoard(no,false);
 		model.addAttribute("board",board);
+		model.addAttribute("pageNum",pageNum);
 		return "views/updateForm";
 	}
 	
 	@PostMapping("/update")
-	public String updateBoard(Board board, HttpServletResponse response, PrintWriter out) {
+	public String updateBoard(Board board, HttpServletResponse response, PrintWriter out,
+			RedirectAttributes reAttrs, @RequestParam(value="pageNum",defaultValue="1")int pageNum) {
 		boolean isPassCheck=boardService.isPassCheck(board.getNo(), board.getPass());
 		if(! isPassCheck) {
 			response.setContentType("text/html; charset=utf-8");
@@ -107,6 +112,8 @@ public class BoardController {
 			return null;
 		}
 		boardService.updateBoard(board);
+		reAttrs.addAttribute("pageNum",pageNum);
+		reAttrs.addFlashAttribute("test1","1회성 파라미터");
 		return "redirect:boardList";
 	}
 }
